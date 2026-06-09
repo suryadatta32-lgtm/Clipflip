fa0bf580764540d3b311789e25f864a0de20ddd6
suryadatta32-lgtm <suryadatta32@gmail.com>
2026-06-09 08:39:25 +0000

    push

--- CLIQ-FLIP/myproject/myproject/settings.py
+++ CLIQ-FLIP/myproject/myproject/settings.py
@@ -132,6 +132,8 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 STATICFILES_DIRS = [
     BASE_DIR / "myapp/static",
 ]

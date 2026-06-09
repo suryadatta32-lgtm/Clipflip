6bfa2a96d34281b2369cba1a985b1b5b535ddafa
suryadatta32-lgtm <suryadatta32@gmail.com>
2026-06-09 13:52:21 +0530

    push

--- CLIQ-FLIP/myproject/myproject/settings.py
+++ CLIQ-FLIP/myproject/myproject/settings.py
@@ -19,7 +19,8 @@ load_dotenv()
 
 SECRET_KEY = os.getenv('SECRET_KEY')
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = ALLOWED_HOSTS = ['clipflip-production.up.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',') ['clipflip-production.up.railway.app', 'localhost', '127.0.0.1']
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -33,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = ['clipflip-production.up.railway.app', 'localhost', '127.0.0.1']
 
 
 # Application definition

84aa84a2744bb67021425c15085e2edb8d661346
suryadatta32-lgtm <suryadatta32@gmail.com>
2026-06-09 13:53:43 +0530

    push

--- CLIQ-FLIP/myproject/myproject/settings.py
+++ CLIQ-FLIP/myproject/myproject/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['clipflip-production.up.railway.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
